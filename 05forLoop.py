b2150137be760f7eedddf97c28f0d5c2cae903b6
jennica-wang <jennica.wang@education.nsw.gov.au>
2021-10-22 09:46:00 +1100

    Finished

---
+++ 05forLoop.py
@@ -0,0 +1,9 @@
+print(" "+"-"*48)
+print("|                                                |")
+print("|    05HelloWorld                                |")
+print("|    Name : Jennica Wang                         |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" "+"-"*48)
+for x in range(1, 500):
+    print(x)

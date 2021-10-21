5bae30723afe312ecde3b3ffb57d039de4678fa6
jennica-wang <jennica.wang@education.nsw.gov.au>
2021-10-21 23:20:47 +1100

    Finished

---
+++ 04GoodTeacher.py
@@ -0,0 +1,12 @@
+print(" "+"-"*48)
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Jennica Wang                         |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" "+"-"*48)
+x = input("Teacher's name (try Mr Horan) ")
+if x == "Mr Horan":
+    print("You are lucky, he is a great teacher.")
+else:
+    print(x+" is an ok teacher")

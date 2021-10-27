fb367bc1ef49d69478cf8db7af4ef9ad734ada4a
jennica-wang <jennica.wang@education.nsw.gov.au>
2021-10-27 12:15:27 +1100

    Finished

---
+++ 06whileLoop.py
@@ -0,0 +1,18 @@
+print(" "+"-"*48)
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Jennica Wang                         |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" "+"-"*48)
+while True:
+    z = input("What is the name of this subject ")
+    if z == "IST":
+        print("")
+        print("")
+        print(" Congratulations!!")
+        print("")
+        print("")
+        break
+    else:
+        print("Not Correct - try again")
